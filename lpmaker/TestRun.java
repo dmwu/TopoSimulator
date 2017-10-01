7fe7c1c8ea2282b1dd1f92fb698dae6b6a04a230
Dingming Wu <dmwu0506@gmail.com>
2017-10-01 14:08:13 -0500

    try multiple instances and not fair throughtput

--- lpmaker/TestRun.java
+++ lpmaker/TestRun.java
@@ -13,13 +13,13 @@ public class TestRun {
     public static void main(String args[]) throws IOException {
 
         for (int i = 0; i <= 20; i+=5) {
-            for (int j = 0; j< 3; j++) {
+            for (int j = 0; j< 4; j++) {
                 FatTreeSigcomm fts = new FatTreeSigcomm(16, i);
-                fts.PrintGraphforMCFFairCondensed("fattree16_" + i +"_"+j+".lp", 1, 0);
+                fts.PrintGraphforMCFFairCondensedAverage("fattree16_" + i +"_"+j+".lp", 1, 0);
                 F10 f10 = new F10(16, i);
-                f10.PrintGraphforMCFFairCondensed("ften16_" + i + "_"+ j + ".lp", 1, 0);
+                f10.PrintGraphforMCFFairCondensedAverage("ften16_" + i + "_"+ j + ".lp", 1, 0);
                 AspenTree asp = new AspenTree(16, 4, 8, i);
-                asp.PrintGraphforMCFFairCondensed("aspen16_" + i +"_" + j + ".lp", 1, 0);
+                asp.PrintGraphforMCFFairCondensedAverage("aspen16_" + i +"_" + j + ".lp", 1, 0);
             }
 
         }
