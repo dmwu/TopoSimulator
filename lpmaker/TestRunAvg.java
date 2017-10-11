fc58f95a59fc1244f8f70368e6b745a58dc72671
Dingming Wu <dmwu0506@gmail.com>
2017-10-11 13:15:30 -0500

    try k=24

--- lpmaker/TestRunAvg.java
+++ lpmaker/TestRunAvg.java
@@ -7,19 +7,21 @@ import lpmaker.graphs.FatTreeSigcomm;
 import java.io.IOException;
 
 /**
- * Created by wdm on 10/2/17.
+ * Created by wdm on 9/22/17.
+ * For aspen trees:   (k,n,d,S) ={(8, 4, 4, 32), (16, 4, 8, 128), (24, 4, 12, 288), (32, 4, 16, 512)}
  */
+
 public class TestRunAvg {
     public static void main(String args[]) throws IOException {
-        int[] myIntArray = {0, 1, 2, 3, 4, 5};
+        int[] myIntArray = {0, 1, 2, 3, 5, 10, 20};
         for (int i : myIntArray) {
             for (int j = 0; j< 6; j++) {
-                FatTreeSigcomm fts = new FatTreeSigcomm(16, i);
-                fts.PrintGraphforMCFFairCondensedAverage("fattree16_" + i +"_"+j+".lp", 1, 0);
-                F10 f10 = new F10(16, i);
-                f10.PrintGraphforMCFFairCondensedAverage("ften16_" + i + "_"+ j + ".lp", 1, 0);
-                AspenTree asp = new AspenTree(16, 4, 8, i);
-                asp.PrintGraphforMCFFairCondensedAverage("aspen16_" + i +"_" + j + ".lp", 1, 0);
+                FatTreeSigcomm fts = new FatTreeSigcomm(24, i);
+                fts.PrintGraphforMCFFairCondensedAverage("fattree24_" + i +"_"+j+".lp", 1, 0);
+                F10 f10 = new F10(24, i);
+                f10.PrintGraphforMCFFairCondensedAverage("ften24_" + i + "_"+ j + ".lp", 1, 0);
+                AspenTree asp = new AspenTree(24, 4, 12, i);
+                asp.PrintGraphforMCFFairCondensedAverage("aspen24_" + i +"_" + j + ".lp", 1, 0);
             }
 
         }
--- lpmaker/TestRunMin.java
+++ lpmaker/TestRunMin.java
@@ -11,15 +11,15 @@ import lpmaker.graphs.*;
  */
 public class TestRunMin {
     public static void main(String args[]) throws IOException {
-        int[] myIntArray = {0, 1, 2, 3, 4, 5};
+        int[] myIntArray = {0, 1, 2, 3, 5, 10, 20};
         for (int i: myIntArray) {
             for (int j = 0; j< 6; j++) {
-                FatTreeSigcomm fts = new FatTreeSigcomm(16, i);
-                fts.PrintGraphforMCFFairCondensed("fattree16_" + i +"_"+j+".lp", 1, 0);
-                F10 f10 = new F10(16, i);
-                f10.PrintGraphforMCFFairCondensed("ften16_" + i + "_"+ j + ".lp", 1, 0);
-                AspenTree asp = new AspenTree(16, 4, 8, i);
-                asp.PrintGraphforMCFFairCondensed("aspen16_" + i +"_" + j + ".lp", 1, 0);
+                FatTreeSigcomm fts = new FatTreeSigcomm(24, i);
+                fts.PrintGraphforMCFFairCondensed("fattree24_" + i +"_"+j+".lp", 1, 0);
+                F10 f10 = new F10(24, i);
+                f10.PrintGraphforMCFFairCondensed("ften24_" + i + "_"+ j + ".lp", 1, 0);
+                AspenTree asp = new AspenTree(24, 4, 12, i);
+                asp.PrintGraphforMCFFairCondensed("aspen24_" + i +"_" + j + ".lp", 1, 0);
             }
 
         }
