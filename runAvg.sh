c681cf4dc6ab4331d76d040e081b85629ec861c3
Dingming Wu <dmwu0506@gmail.com>
2017-12-08 01:02:33 -0600

    run in parallel

--- runAvg.sh
+++ runAvg.sh
@@ -8,26 +8,27 @@ for topo in 1 2 3
         do
         for failCount in 0 1 3 5 10 20
             do
-            for trial in 1 2 3 4
+            for trial in 1 2 3
                 do
                 java lpmaker.TestRunAvg "$topo" "$failurePos" "$failCount" "$trial" &
             done
+            wait
+            for entry in ./*.lp
+                do
+                echo "$entry"
+                gurobi_cl Threads=10 Method=2 Crossover=0 ResultFile=${entry}'.sol' ${entry} &
+            done
+            wait
+            rm ./*.lp
+
+            for entry in ./*.lp.sol
+                do
+                echo "$entry"
+                line=$(head -n 1 "$entry")
+                echo -e "$entry $line" >> "finalAvg.txt"
+            done
+            rm ./*.lp.sol
         done
     done
 done
 
-for entry in ./*.lp
-    do
-    echo "$entry"
-    gurobi_cl Threads=10 Method=2 Crossover=0 ResultFile=${entry}'.sol' ${entry} &
-done
-wait
-rm ./*.lp
-
-for entry in ./*.lp.sol
-    do
-    echo "$entry"
-    line=$(head -n 1 "$entry")
-    echo -e "$entry $line" >> "finalAvg.txt"
-done
-rm ./*.lp.sol
