299e4c70f89240444d20ac397dcda7bd6fc8146f
Dingming Wu <dmwu0506@gmail.com>
2017-12-17 23:28:17 -0600

    fix bug when remove links on the same switch

--- runMin.sh
+++ runMin.sh
@@ -12,13 +12,13 @@ for trafficMode in 0 2 11 15
         for entry in ./*.lp
             do
             echo "$entry"
-            #gurobi_cl Threads=12 Method=2 Crossover=0 ResultFile=$entry'.sol' $entry
+            gurobi_cl Threads=12 Method=2 Crossover=0 ResultFile=$entry'.sol' $entry
         done
         rm ./*.lp
 
         for failureMode in 1 2 3 4
             do
-            for failCount in 20
+            for failCount in 1 3 5 10 20
                 do
                 for trial in 1 2 3 4 5 6 7 8
                     do
@@ -27,7 +27,7 @@ for trafficMode in 0 2 11 15
                 for entry in ./*.lp
                     do
                     echo "$entry"
-                    #gurobi_cl Threads=12 Method=2 Crossover=0 ResultFile=$entry'.sol' $entry
+                    gurobi_cl Threads=12 Method=2 Crossover=0 ResultFile=$entry'.sol' $entry
                 done
                 rm ./*.lp
             done
